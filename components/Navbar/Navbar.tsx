d297d612267dcdf0eff2e0b2617baeb44d89b1f2
004abvy <abvy7661@gmail.com>
2026-09-08 19:39:15 +0500

    Add Menu heading inline inside scrollable list above Home link

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -156,6 +156,8 @@ export default function Navbar() {
         </button>
 
         <div className={styles.navLinksScroll}>
+          <h3 className={styles.menuHeading}>Menu</h3>
+
           <Link href="/" prefetch={false} className={`${styles.navLink} ${pathname === '/' ? styles.active : ''}`} onClick={() => setMobileMenuOpen(false)}>
             <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path></svg>
             Home
