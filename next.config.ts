4c36c6c629dea154b278bd457300fc1b8d8f07f1
004abvy <abvy7661@gmail.com>
2026-09-17 16:38:40 +0500

    Fix useRef import in HeroCarousel and remove deprecated eslint config in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -13,12 +13,6 @@ const withPWA = withPWAInit({
 // @ts-ignore
 const nextConfig: NextConfig = {
   turbopack: {},
-  // @ts-ignore
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
