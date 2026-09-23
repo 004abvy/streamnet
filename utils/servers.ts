fdf09ffdaed40698c76a10facc06dbff77140b87
004abvy <abvy7661@gmail.com>
2026-09-23 20:21:48 +0500

    Update RiveStream to use iframe embed API

--- utils/servers.ts
+++ utils/servers.ts
@@ -29,7 +29,7 @@ export const SERVERS: StreamingServer[] = [
   {
     id: 'rivestream',
     name: 'RiveStream',
-    category: 'direct',
+    category: 'iframe',
     enabled: true,
     buildUrl: ({ tmdbId, type, season, episode }) => 
       type === 'movie'
